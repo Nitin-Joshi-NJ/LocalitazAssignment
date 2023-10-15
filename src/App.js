e637c772ea73cb30bf08b7b4e24b9f5e664c6534
Nitin Joshi <jnitin18669@gmail.com>
2023-10-15 16:49:19 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const rotateImage = useSpring({
+    transform: 'rotate(360deg)',
+    from: { transform: 'rotate(0deg)' },
+    config: { duration: 8000, loop: true },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header >
+        <h1>Step into Style</h1>
+        <p>Discover our latest collection of comfortable and fashionable shoes.</p>
       </header>
+      <section className="shoe-section">
+        <animated.div className="shoe-image" style={rotateImage}></animated.div>
+        <div className="shoe-details" >
+          <h2>Comfort Redefined</h2>
+          <p>Step into comfort with our premium collection of shoes designed for your everyday adventures.</p>
+          <a href="#shop" className="cta-button">Shop Now</a>
+        </div>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
